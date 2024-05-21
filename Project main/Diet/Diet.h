cbc378395f07f3b0f9d11fdffec601e5777b0adc
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-05-21 10:08:40 +0300

    Update Diet.h

--- Project main/Diet/Diet.h
+++ Project main/Diet/Diet.h
@@ -1,1 +1,16 @@
+#ifndef APPLICATION_DIET_H
+#define APPLICATION_DIET_H
+
+/*
+class Diet {
+public:
+    virtual void create_diet_plan() = 0;
+};
+ */
+class Diet {
+public:
+    virtual void get_diet_plan() = 0;
+};
+
+#endif
 

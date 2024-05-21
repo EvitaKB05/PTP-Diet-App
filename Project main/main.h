4ec5bd11edbc1afbcb2faae43f31f1f55847d476
Karina-Podgornova <149087540+Karina-Podgornova@users.noreply.github.com>
2024-05-21 10:02:44 +0300

    Update main.h

--- Project main/main.h
+++ Project main/main.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <locale>
+#include <iomanip>
+
+
+#include "CalorieCounter/CalorieCounter.h"
+#include "Person/Person.h"
+//#include "dataBase/dataBase.h"
+#include "Diet/Diet.h"
+#include "WeightGainDiet/WeightGainDiet.h"
+#include "WeightLossDiet/WeightLossDiet.h"
+//#include "HealthyWeightDiet/HealthyWeightDiet.h"
 

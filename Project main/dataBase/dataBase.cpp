59f90917ba34645e910c44c13c0f062141fcd364
Karina-Podgornova <149087540+Karina-Podgornova@users.noreply.github.com>
2024-05-21 10:14:37 +0300

    Update dataBase.cpp

--- Project main/dataBase/dataBase.cpp
+++ Project main/dataBase/dataBase.cpp
@@ -1,1 +1,19 @@
+#include "dataBase.h"
+
+std::vector<std::string> foodCategoriesNames = { "Other", "Milks", "Meats", "Cans", "Vegetables", "Mushrooms", "Fruits",
+                                                 "Eggs", "Nuts", "Fish", "FishProducts", "FishCans",	"Oils", "Bread", "Sweets", "Juices" };
+
+DataBase::DataBase()
+{
+    catalog.clear();
+    getDataBase();
+    printDataBase();
+}
+
+void DataBase::printDataBase()
+{
+    for (auto& product : catalog)
+        std::cout << product.name << ' ' << product.proteins << ' ' << product.fats << ' ' << product.carbs << ' ' <<
+                  product.energy << std::endl;
+}
 

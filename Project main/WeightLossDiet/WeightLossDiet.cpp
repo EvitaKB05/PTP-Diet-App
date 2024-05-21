d3461ab76500bce71ba8aad27b4a3840805a6a37
Karina-Podgornova <149087540+Karina-Podgornova@users.noreply.github.com>
2024-05-21 10:05:18 +0300

    Update WeightLossDiet.cpp

--- Project main/WeightLossDiet/WeightLossDiet.cpp
+++ Project main/WeightLossDiet/WeightLossDiet.cpp
@@ -1,1 +1,20 @@
+#include "WeightLossDiet.h"
+/*
+void WeightLossDiet::get_target_weight_loss() {
+    std::cout << "Please, enter how many kilos you want to loose: ";
+    std::cin >> target_weight_loss;
+}
+
+void WeightLossDiet::create_diet_plan() {
+    char breakfast[50], lunch[50], dinner[50];
+    std::cout << "Enter breakfast plan for losing weight: ";
+    std::cin >> breakfast;
+    std::cout << "Enter lunch plan for losing weight: ";
+    std::cin >> lunch;
+    std::cout << "Enter dinner plan for losing weight: ";
+    std::cin >> dinner;
+
+    std::cout << "Diet plan for losing weight created:\nBreakfast: " << breakfast << "\nLunch: " << lunch << "\nDinner: " << dinner << std::endl;
+}
+*/
 

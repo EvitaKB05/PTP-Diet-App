d071bf0327de341ddb1d8f4a82fbf7b33d6094fe
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-05-21 09:13:12 +0300

    Update Person.h

--- Project main/Person/Person.h
+++ Project main/Person/Person.h
@@ -1,1 +1,84 @@
+#ifndef APPLICATION_PERSON_H
+#define APPLICATION_PERSON_H
+class Person {
+private:
+    char* name;
+    char gender;
+    double height;
+    double weight;
+    std::string DietType;
+public:
+    Person( char* name, char gender, double height, double weight, std::string DietType);
+    /*
+    Person() {
+        name = new char[100];
+    }
+     */
+    Person();
 
+    Person(const Person &other);
+    void get_user_data();
+    ~Person();
+    //char analyze_weight();
+    char *GetName() const;
+    char GetGender() const;
+    double GetHeight() const;
+    double GetWeight() const;
+    std::string GetDietType() const;
+    void SetName(char *name);
+    void SetGender(char gender);
+    void SetHeight(double height);
+    void SetWeight(double weight);
+    void SetDietType(double DietType);
+    void checkWeightStatus();
+    bool is_overweight() {
+        return (weight > 0.75 * height);
+    }
+
+    bool is_underweight() {
+        return (weight < 0.5 * height);
+    }
+
+    static void trackProgress(const Person &user);
+};
+
+/*
+class Person {
+public:
+    char* name;
+    char gender;
+    int height;
+    int weight;
+
+    Person() {
+        name = new char[100];
+    }
+
+
+    ~Person() {
+        delete[] name;
+    }
+
+    void get_data() {
+        cout << "Enter Name:";
+        cin >> name;
+        cout << "Enter Gender (M/F/O):";
+        cin >> gender;
+        cout << "Enter Height:";
+        cin >> height;
+        cout << "Enter Weight:";
+        cin >> weight;
+    }
+
+    bool is_overweight() {
+        return (weight > 0.75 * height);
+    }
+
+    bool is_underweight() {
+        return (weight < 0.5 * height);
+    }
+*/
+
+
+
+#endif

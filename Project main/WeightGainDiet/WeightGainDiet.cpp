95884db7e3eceabd29e63d37b4eb4620fa1e4476
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-05-21 09:54:06 +0300

    Update WeightGainDiet.cpp

--- Project main/WeightGainDiet/WeightGainDiet.cpp
+++ Project main/WeightGainDiet/WeightGainDiet.cpp
@@ -1,1 +1,7 @@
-
+#include "WeightGainDiet.h"
+/*
+void WeightGainDiet::get_target_weight_gain() {
+    std::cout << "Please, enter how many kilos you want to gain: ";
+    std::cin >> target_weight_gain;
+}
+ */

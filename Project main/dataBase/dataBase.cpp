4022aa4b6b0e8851b55f417f85a403969cb3f104
Karina-Podgornova <149087540+Karina-Podgornova@users.noreply.github.com>
2024-05-21 10:17:33 +0300

    Update dataBase.cpp

--- Project main/dataBase/dataBase.cpp
+++ Project main/dataBase/dataBase.cpp
@@ -213,3 +213,20 @@ void Food::edit(Food* arr, int n, int is_interactive){
         }
     }
 }
+id Food::print_dbase(const Food* arr, int n){
+    for (int i = 0; i < n; i++){
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
+void Food::sort_dbase(Food* arr, int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = 0; j < n - i - 1; j++){
+            if(arr[j] < arr[j + 1]){
+                Food temp{arr[j]};
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}

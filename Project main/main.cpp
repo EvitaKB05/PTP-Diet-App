988739c2fd8425296c82329a6e9628aec2b91d0f
Karina-Podgornova <149087540+Karina-Podgornova@users.noreply.github.com>
2024-05-21 10:01:42 +0300

    Update main.cpp

--- Project main/main.cpp
+++ Project main/main.cpp
@@ -55,4 +55,17 @@ int interactive(){
     return 0;
 }
 
+void createDietForOverweight() {
+    char breakfast[50], lunch[50], dinner[50];
+    std::cout << "Enter breakfast plan for losing weight: ";
+    std::cin >> breakfast;
+    std::cout << "Enter lunch plan for losing weight: ";
+    std::cin >> lunch;
+    std::cout << "Enter dinner plan for losing weight: ";
+    std::cin >> dinner;
+
+    std::cout << "Diet plan for losing weight created:\nBreakfast: " << breakfast << "\nLunch: " << lunch << "\nDinner: " << dinner << std::endl;
+}
+
+
 

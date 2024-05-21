bec7d3c1d36917867e395e2724624862b955ad13
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-05-21 09:32:12 +0300

    Update WeightGainDiet.h

--- Project main/WeightGainDiet/WeightGainDiet.h
+++ Project main/WeightGainDiet/WeightGainDiet.h
@@ -4,3 +4,12 @@
 #include <iostream>
 #include "../main.h"
 #include "../Diet/Diet.h"
+class WeightGainDiet : public Diet {
+public:
+    int target_weight_gain;
+
+    void get_target_weight_gain();
+
+    //void createDietForGainWeight();
+    void create_diet_plan();
+};

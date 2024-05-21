99329470495332cd6cbee033ef519b69ec3ab649
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-05-21 09:33:18 +0300

    Update WeightGainDiet.h

--- Project main/WeightGainDiet/WeightGainDiet.h
+++ Project main/WeightGainDiet/WeightGainDiet.h
@@ -4,4 +4,24 @@
 #include <iostream>
 #include "../main.h"
 #include "../Diet/Diet.h"
+class WeightGainDiet : public Diet {
+public:
+    void get_diet_plan() override {
+        std::cout << " Diet for Weight Gain" << std::endl;
+        get_meal_data("Breakfast");
+        get_meal_data("Lunch");
+        get_meal_data("Dinner");
+    }
+
+    //static void createDietForGainWeight();
+
+private:
+    void get_meal_data(const char* meal_name) {
+        std::cout << meal_name << ":" << std::endl;
+        // ... получение информации о продуктах
+    }
+
+};
+
+#endif //APPLICATION_WEIGHTGAINDIET_H
 
